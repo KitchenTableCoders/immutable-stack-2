ca7f6e1e24276267e34ee0bd5975ca4108cfc86a
dnolen <dnolen@cognitect.com>
2015-10-16 17:58:41 -0400

    fix exercises

--- om/breakout2/src/breakout2/core.cljs
+++ om/breakout2/src/breakout2/core.cljs
@@ -24,12 +24,12 @@
 (defmethod read :address-book
   [{:keys [state]} key params]
   (let [st @state]
-    {:value (into [] (map #(get-in st %)) (get st k))}))
+    {:value (into [] (map #(get-in st %)) (get st key))}))
 
 (defmethod read :friends
   [{:keys [state]} key params]
   (let [st @state]
-    {:value (into [] (map #(get-in st %)) (get st k))}))
+    {:value (into [] (map #(get-in st %)) (get st key))}))
 
 (defui AddressBookEntry
   static om/Ident
@@ -37,7 +37,7 @@
     [:person/by-name name])
   static om/IQuery
   (query [this]
-    [:name :age]))
+    [:name :email :phone-number]))
 
 (defui Friend
   static om/Ident
@@ -45,25 +45,28 @@
     [:person/by-name name])
   static om/IQuery
   (query [this]
-    [:name :email :phone-number]))
+    [:name :age]))
 
 (defui MyApp
   static om/IQuery
   (query [this]
-    [{:address-book (om/get-query AddressBookEntry)}
-     {:friends (om/get-query Friend)}]))
+    [{:friends (om/get-query Friend)}
+     {:address-book (om/get-query AddressBookEntry)}]))
 
 (comment
-  ;; Exercise 1: normalize the data by running the following
+  ;; Exercise 1: Check the MyApp query
+  (om/get-query MyApp)
+  ;; Exercise 2: normalize the data by running the following
+  ;;   normalize uses the query on the first argument to
+  ;;   transform the second argument
   (pprint/pprint (om/normalize MyApp init-data))
-  ;; Exericse 2: normalize the data by running the following
+  ;; Exercise 3: normalize the data by running the following
   ;;   What's the difference?
   (pprint/pprint (om/normalize MyApp init-data true))
-  ;; Exercise 3: put the normalized data into an atom and
+  ;; Exercise 4: put the normalized data into an atom and
   ;;   store the atom in a var called "norm"
-  ;; Exercise 4: make a parser using the supplied "read" function
+  ;; Exercise 5: make a parser using the supplied "read" function
   ;;   put this in var called "parser"
-  ;; Exercise 5: parse the data back into a tree with the following
-  ;;   expression
-  (pprint/pprint (parser {:state norm} (om/get-query MyApp)))
+  ;; Exercise 6: parse the data back into a tree with the following
+  ;;   expression. What's interesting about the result?
   )
