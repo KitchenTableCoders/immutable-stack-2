92b068480605674e44e3197373eac3f26d9da907
dnolen <dnolen@cognitect.com>
2015-10-16 15:52:44 -0400

    More exercises

--- om/breakout1/src/breakout1/core.cljs
+++ om/breakout1/src/breakout1/core.cljs
@@ -3,11 +3,12 @@
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]))
 
+(enable-console-print!)
+
 (defui Hello
   Object
   (render [this]
-    (dom/div nil
-      "Hello, world!")))
+    (dom/div nil "Hello, world!")))
 
 (def hello (om/factory Hello))
 
@@ -15,5 +16,12 @@
 
 ;; Exercise 1: Change the text to something else
 ;; Exercise 2: Change the background color to something else
-;; Exercise 3: Pass the text as props
+;; Exercise 3: Pass the text as props and use this
+;; Exercise 4: Add componentDidMount life cycle method, print something
+;;   Make a change
+;; Exercise 5: Add componentWillUpdate life cycle method, print something
+;;   Make a change, why doesn't this print?
+;; Exercise 6: Add ^:once before Hello
+;; Exercise 7: Change the props, which life cycle methods print, which ones
+;;   don't? Can you explain why?
 ;; Bonus: Render a list of of Hello components
